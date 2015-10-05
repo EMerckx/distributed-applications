5bf0c4b50c1e2c2d36c34ecb49e2d96524e78535
EMerckx <ewoutmerckx.pro@gmail.com>
2015-10-05 23:03:45 +0200

    exercise 2 of lab 1 as good as completed

--- lab01/02/README.md
+++ lab01/02/README.md
@@ -5,3 +5,14 @@ We openen eerst Netbeans. Daarna maken we een nieuw project aan.
 ```
 File > New Project > Java > Java Application
 ```
+
+Dan maken we van het project een web service client.
+
+```
+Rechtermuis op project02 > New > Web Service Client
+```
+
+Hier vullen we de WSDL URL in van de web service: http://services.aonaware.com/DictService/DictService.asmx?WSDL.
+We nemen ook als package project02, en dan klikken we op finish.
+Er word nu voor ons de nodige files gegenereerd, daarna kunnen we beginnen met coderen.
+
--- lab01/02/project02/src/project02/Project02.java
+++ lab01/02/project02/src/project02/Project02.java
@@ -15,7 +15,25 @@ public class Project02 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // create a service
+        DictService dictService = new DictService();
+        
+        // create a soap
+        DictServiceSoap dictServiceSoap = dictService.getDictServiceSoap();
+        
+        // set the given word
+        String given_word = "servizes";
+        
+        // get the corrections of the given word
+        ArrayOfDictionaryWord arr = dictServiceSoap.match(given_word, "lev");
+        
+        // handle the output
+        System.out.println("word: servizes");
+        System.out.print("corrections: ");
+        for(int i=0; i<arr.getDictionaryWord().size(); i++){
+            System.out.print(arr.getDictionaryWord().get(i).word);
+        }
+        System.out.println(" ");
     }
     
 }
